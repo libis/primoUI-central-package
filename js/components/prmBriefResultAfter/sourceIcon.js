cbffa872f31e3095bbd6d196233e7a4120da499f
Tom Vanmechelen <tom.vanmechelen@libis.be>
2018-06-08 12:02:55 +0200

    source icon - lowercase source_id

--- js/components/prmBriefResultAfter/sourceIcon.js
+++ js/components/prmBriefResultAfter/sourceIcon.js
@@ -14,7 +14,7 @@ class SourceIconController {
 
     self.sourceid = null;
     if ('sourceid' in parentCtrl.item.pnx.control) {
-      self.sourceid = parentCtrl.item.pnx.control.sourceid[0];
+      self.sourceid = parentCtrl.item.pnx.control.sourceid[0].toLowerCase();
 
       if (iconConfSources.includes(self.sourceid)) {
         self.icon = iconConf.find(function(x) {
