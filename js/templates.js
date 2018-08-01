103241cadbcbcdd8be3374e1ef9bcdfe4bec2f3a
Tom Vanmechelen <tom.vanmechelen@libis.be>
2018-08-01 16:12:52 +0200

    view dependent template system (similar to components)

---
+++ js/templates.js
@@ -0,0 +1,36 @@
+/*
+  Replace templates.
+
+  Replace the default primo templates http://localhost:8003/primo-explore/lib/templates.js
+  with custom templates
+
+  KULeuven/LIBIS (c) 2018
+  Tom Vanmechelen
+*/
+import './utils'
+
+/* import your templates */
+
+import changeLangWithoutUserAreaHTML from '../html/templates/change-lang-without-user-area.html'
+
+export default class Templates {
+    static get all() {
+        /*
+          id = templateId in the templateCache 
+          template = the imported tempalte
+          enabled = true/false should the component be created
+          enableInView = regex to define in which views the template has to be replaced.
+    
+          ex. {id: 'components/search/topbar/userArea/user-area.html', template: userAreaHTML, enabled: true, enableInView: '.*'}
+          results in:
+            $templateCache.put('components/search/topbar/userArea/user-area.html',userAreaHTML);
+        */
+        return [{
+                id: 'components/search/topbar/userArea/user-area.html',
+                template: changeLangWithoutUserAreaHTML,
+                enabled: true,
+                enableInView: 'VDIC'
+            }
+        ].filter((template) => (template.enabled && new RegExp(template.enableInView).test(window.appConfig.vid)));
+    }
+}
