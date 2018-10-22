0dd586b7f004b0d4ce21ef57e8f11109f03d401e
Tom Vanmechelen <tom.vanmechelen@libis.be>
2018-10-22 14:31:22 +0200

    optimize altmetrics

--- js/components/prmFullViewAfter/altmetricsBadge.js
+++ js/components/prmFullViewAfter/altmetricsBadge.js
@@ -8,41 +8,20 @@ class AltmetricsBadgeController {
 
   constructor($scope, $element, $compile, $rootScope, $window, $timeout, AltmetricsService) {
     let self = this;
-    this.$scope = $scope;
-    this.$rootScope = $rootScope;
-    this.$element = $element;
-    this.$window = $window;
-    this.$timeout = $timeout;
-    this.$scope = $scope;
-//    this.AltmetricsService = AltmetricsService;
-    var parentCtrl = this.parentCtrl.parentCtrl;
-    let item = parentCtrl.item;
-    self.recordid = item.pnx.control.recordid[0];
-    self.doi = '';
-
-    if (item && item.pnx && item.pnx.addata && item.pnx.addata.doi) {
-      self.doi = self.parentCtrl.parentCtrl.item.pnx.addata.doi[0];
-      AltmetricsService.addAltmetrics('#full_altmetrics_' + self.recordid).then(function (result) {
-        if (result) {
-          var altmetricsSection = {
-            scrollId: "altmetrics",
-            serviceName: "altmetrics",
-            title: "brief.results.tabs.Altmetrics"
-          };
-          Promise.resolve(  parentCtrl.fullViewService.getServices(parentCtrl.item, parentCtrl.originator) ).then(function(services) {
-            services.splice(services.length, 10, altmetricsSection);
-          })
-        }
-      });
-    }
+    self.$element = $element;
+    self.$scope = $scope;
+    self.AltmetricsService = AltmetricsService;
   }
 
   $onInit() {
-    //polyfill
-    if (!Element.prototype.matches)
-    Element.prototype.matches = Element.prototype.msMatchesSelector ||
-                                Element.prototype.webkitMatchesSelector;
+    let self = this;
+    self.parentCtrl = this.parentCtrl.parentCtrl;
+    self.item = self.parentCtrl.item;
+    self.recordid = self.item.pnx.control.recordid[0];
+    self.doi = '';
 
+    //polyfill
+    if (!Element.prototype.matches) Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
     if (!Element.prototype.closest)
         Element.prototype.closest = function(s) {
             var el = this;
@@ -53,10 +32,31 @@ class AltmetricsBadgeController {
             } while (el !== null && el.nodeType === 1);
             return null;
         };
+    //end polyfill
 
-    let doi = this.doi
-    let self = this;
-    if (doi !== '') {
+    if (self.item && self.item.pnx && self.item.pnx.addata && self.item.pnx.addata.doi) {
+        self.doi = self.item.pnx.addata.doi[0];
+        self.AltmetricsService.addAltmetrics('#full_altmetrics_' + self.recordid).then(function (result) {
+
+        if (result) {
+          var altmetricsSection = {
+            scrollId: "altmetrics",
+            serviceName: "altmetrics",
+            title: "brief.results.tabs.Altmetrics"
+          };
+
+          Promise.resolve( self.parentCtrl.fullViewService.getServices( self.parentCtrl.item, self.parentCtrl.originator) ).then(
+            function(services) {
+              // Add to services if not alread in the list
+              if ( services.findIndex(service => service.scrollId === "altmetrics") < 0 ){
+                services.splice(services.length, 10, altmetricsSection);              
+              }
+          })
+        }
+      });
+    }
+    
+    if ( self.doi !== '' ) {
       var unbindWatcher = this.$scope.$watch(function () {
         //return self.$element[0].parentElement.parentElement.querySelector('h4[translate="brief.results.tabs.Altmetrics"]');
         return self.$element[0].closest('#fullView').querySelector('h4[translate="brief.results.tabs.Altmetrics"]')
@@ -65,9 +65,9 @@ class AltmetricsBadgeController {
           //Get the section body associated with the value we're watching
           let altmetricsContainer = newVal.closest('prm-full-view-service-container').querySelector('div.section-body');
           let altmetricsResult = self.$element[0];
-
           if (altmetricsContainer && altmetricsContainer.appendChild && altmetricsResult) {
             altmetricsContainer.appendChild(altmetricsResult);
+            window._altmetric_embed_init(altmetricsContainer);
           }
           unbindWatcher();
         }
