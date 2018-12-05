27a4e4c7109a58822770d598f58c31717350933b
Joris <joris.lambrechts@kuleuven.be>
2018-12-05 15:31:55 +0100

    add interceptor on dbsearch api calls for regional views

--- js/utils.js
+++ js/utils.js
@@ -1,11 +1,38 @@
 /*
-  General 
+  General
 
   KULeuven/LIBIS (c) 2017
   Mehmet Celik
 */
 String.prototype.toCamelCase = function () {
   return this.split('-').map((d, i, a) => i > 0 ? d.charAt(0).toUpperCase() + d.slice(1) : d).join('');
+}
+
+//intercept api calls for database search and change to KUL (only regional views)
+var reg_views = ["KULeuven_TMOREM", "KULeuven_TMOREK", "KULeuven_UCLL", "KULeuven_LUCA", "KULeuven_ODISEE"];
+if (reg_views.includes(window.appConfig.vid)) {
+  let app = angular.module('viewCustom', [])
+    .factory('httpCallInterceptor', [() => {
+      var httpCallInterceptor = {
+        request: function(config){
+          if (config.url.includes('pnxs') && !config.url.includes('delivery') && config.params) {
+            //console.log(config);
+
+            if(config.params.databases){
+              config.params.inst = 'KUL';
+              config.params.vid = 'KULeuven';
+            }
+
+            return config;
+          }
+          return config;
+        }
+      }
+      return httpCallInterceptor;
+    }])
+    .config(['$httpProvider', ($httpProvider) => {
+      $httpProvider.interceptors.push('httpCallInterceptor');
+    }]);
 }
 
 /*
