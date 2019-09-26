c19b3ebc52e6bfc53c5176bdcfdf4692b3c37c47
Joris <joris.lambrechts@kuleuven.be>
2019-09-26 14:15:58 +0200

    intercept ejournals

--- js/factories/dbSearchHttpCallInterceptor.js
+++ js/factories/dbSearchHttpCallInterceptor.js
@@ -3,7 +3,7 @@ export let dbSearchHttpCallInterceptor =  [() => {
     request: function(config){
       if (config.url.includes('pnxs') && !config.url.includes('delivery') && config.params) {
         //console.log(config);
-        if(config.params.databases){
+        if(config.params.databases || config.params.journals){
           config.params.inst = 'KUL';
           config.params.vid = 'KULeuven';
         }
