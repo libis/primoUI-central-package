3c263e3572846505bdd2bf6ff59f0e306ad3d872
Joris <joris.lambrechts@kuleuven.be>
2019-12-12 10:00:46 +0100

    enable ez proxy when off campus

--- js/components/prmFacetExactAfter/searchAlsoBody.js
+++ js/components/prmFacetExactAfter/searchAlsoBody.js
@@ -3,7 +3,15 @@ import searchAlsoBodyHTML from './searchAlsoBody.html'
 class SearchAlsoBodyController {
   constructor($location){
     this.location = $location;
-    this.targets = this._targets();
+    let url ="";
+    Primo.user.then(user => {
+      let campus = '';
+      campus = user.isOnCampus();
+      if(!campus){
+        url = 'http://kuleuven.ezproxy.kuleuven.be/login?url=';
+      }
+      this.targets = this._targets(url);
+    });
   }
 
   get search() {
@@ -14,10 +22,11 @@ class SearchAlsoBodyController {
     return this.parentCtrl.parentCtrl.facetGroup.name;
   }
 
-  _targets() {
+  _targets(url) {
+
     return [{
       "name": "UniCat",
-      "url": "https://unicat.be/uniCat?func=search&uiLanguage=en&query=",
+      "url": url + "https://unicat.be/uniCat?func=search&uiLanguage=en&query=",
       "img": "/primo-explore/custom/CENTRAL_PACKAGE/img/unicat_icon.png",
       "tooltip": "lbs.nui.search_also.tooltip.unicat",
       mapping: function mapping(search) {
